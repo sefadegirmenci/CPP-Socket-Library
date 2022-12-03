d706b75c268f85ae7424e96a8e5d619f39fa5a8a
zaqim <hdegirmenci18@ku.edu.tr>
2022-12-03 22:42:21 +0300

    Implement secure receive"
    Not to lose packets that are not sent in the first run

--- src/connection.cpp
+++ src/connection.cpp
@@ -60,14 +60,14 @@ extern "C"
         }
 
         /* A structure that contains the address of the server. */
-        struct sockaddr_in serv_addr; 
+        struct sockaddr_in serv_addr;
 
         /* Setting the socket address. */
         bzero((char *)&serv_addr, sizeof(serv_addr));
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_addr.s_addr = INADDR_ANY;
         serv_addr.sin_port = htons(port);
-        
+
         /* Setting the socket options. */
         int enable = 1;
         if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
@@ -86,23 +86,70 @@ extern "C"
         return sockfd;
     }
 
-    int recv_msg(int sockfd, std::string *message)
+    static auto read_n(int fd, char *buffer, size_t n) -> size_t
     {
-
-        /* Receiving the message from the socket */
-        char buffer[MAXBUFFERSIZE];
-        bzero(buffer, MAXBUFFERSIZE - 1);
-        long n = recv(sockfd, buffer, sizeof(buffer), 0);
-        
-        if (n < 0)
+        size_t bytes_read = 0;
+        size_t retries = 0;
+        constexpr size_t max_retries = 10000;
+        while (bytes_read < n)
         {
-            perror("Error reading socket in recv_msg");
-            return -1;
+            auto bytes_left = n - bytes_read;
+            auto bytes_read_now = recv(fd, buffer + bytes_read, bytes_left, 0);
+            // negative return_val means that there are no more data (fine for non
+            // blocking socket)
+            if (bytes_read_now == 0)
+            {
+                if (retries >= max_retries)
+                {
+                    return bytes_read;
+                }
+                ++retries;
+                continue;
+            }
+            if (bytes_read_now > 0)
+            {
+                bytes_read += bytes_read_now;
+                retries = 0;
+            }
         }
-        
-        buffer[n] = '\0';
-        *message = buffer;
-        return 0;
+        return bytes_read;
+    }
+    
+    auto secure_recv(int fd) -> std::pair<size_t, std::unique_ptr<char[]>>
+    {
+        char dlen[4];
+
+        if (auto byte_read = read_n(fd, dlen, length_size_field);
+            byte_read != length_size_field)
+        {
+            debug_print("[{}] Length of size field does not match got {} expected {}\n",
+                        __func__, byte_read, length_size_field);
+            return {0, nullptr};
+        }
+
+        auto actual_msg_size_opt = destruct_message(dlen, length_size_field);
+        if (!actual_msg_size_opt)
+        {
+            debug_print("[{}] Could not get a size from message\n", __func__);
+            return {0, nullptr};
+        }
+
+        auto actual_msg_size = *actual_msg_size_opt;
+        auto buf = std::make_unique<char[]>(static_cast<size_t>(actual_msg_size) + 1);
+        buf[actual_msg_size] = '\0';
+        if (auto byte_read = read_n(fd, buf.get(), actual_msg_size);
+            byte_read != actual_msg_size)
+        {
+            debug_print("[{}] Length of message is incorrect got {} expected {}\n",
+                        __func__, byte_read, actual_msg_size);
+            return {0, nullptr};
+        }
+
+        if (actual_msg_size == 0)
+        {
+            debug_print("[{}] wrong .. {} bytes\n", __func__, actual_msg_size);
+        }
+        return {actual_msg_size, std::move(buf)};
     }
 
     int send_msg(int sockfd, std::string message)
